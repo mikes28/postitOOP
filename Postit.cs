5900efa1199fa5dddb038e764bafa6424d11a529
mikes28 <mikeskrisztian06@gmail.com>
2025-01-24 12:45:18 +0100

    first commit

---
+++ Postit.cs
@@ -0,0 +1,24 @@
+﻿namespace postitOOP;
+
+internal class Postit
+{
+    private string color;
+    private string text;
+
+    public Postit(string color)
+    {
+        this.color = color;
+        this.text = "";
+    }
+
+    public string Color { get => color; }
+    public string Text
+    {
+        get => text;
+        set => text = (text + value).Substring(100);
+    }
+    public override string ToString()
+    {
+        return $"{this.color} postit, tartalom: {this.text}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using postitOOP;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Postit postit1= new("red");
+        Postit postit2= new("green");
+        Postit postit3= new("yellow");
+
+        postit1.Text="asdasd";
+        postit1.Text="asdasd";
+        postit2.Text="asdasdasdasd";
+
+        postit3.Text="asdasdasdasdasdasd";
+
+        System.Console.WriteLine(postit1);
+        System.Console.WriteLine(postit2);
+        System.Console.WriteLine(postit3);
+
+    }
+}
